09cb6037a27262123344916c60fa8f2d2aa76a16
aebratt <aebratt@uw.edu>
2020-01-30 20:30:48 -0800

    example batch script

---
+++ Hyak_simple_batch.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+## Job Name
+#SBATCH --job-name=simple_test
+## Allocation Definition
+#SBATCH --account=stf
+#SBATCH --partition=stf
+## Resources
+## Nodes
+#SBATCH --nodes=1      
+## Tasks per node (Slurm assumes you want to run 28 tasks, remove 2x # and adjust parameter if needed)
+#SBATCH --ntasks-per-node=1
+## Set number of cores per task to allow multithreading
+#SBATCH --cpus-per-task=28
+## Walltime (eight hours)
+#SBATCH --time=8:00:00
+# E-mail Notification, see man sbatch for options
+ 
+
+##turn on e-mail notification
+
+#SBATCH --mail-type=ALL
+
+#SBATCH --mail-user=UWNETID@uw.edu
+
+
+## Memory per node
+#SBATCH --mem=100G
+## Specify the working directory for this job
+#SBATCH --chdir=/gscratch/stf/UWNETID/qermserver-hyak-tutorial
+
+/gscratch/stf/aebratt/fish_cpp_hyak/bin/headless
